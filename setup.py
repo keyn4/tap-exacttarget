c49fa7b73cb1bd17858a4199b9e4de7706312a42
Hassan Syyid <hassansyyid@gmail.com>
2023-09-21 14:48:53 -0400

    Change CLI entrypoint

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     },
     entry_points='''
     [console_scripts]
-    tap-exacttarget=tap_exacttarget:main
+    tap-salesforce-marketing=tap_exacttarget:main
     ''',
     packages=find_packages(),
     package_data={
